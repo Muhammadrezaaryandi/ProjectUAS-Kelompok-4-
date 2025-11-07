bb0dc76d3f574025e0839450d5d89cc5052b2fad
Muhammadrezaaryandi <ARYANDI@mhs.usk.ac.id>
2025-11-07 11:40:42 +0700

    prtama

--- cobalagiah.c
+++ cobalagiah.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main (){
-    printf("hello world");
+int main() {
+    char nama[50];
+
+    printf("Masukkan nama kamu: ");
+    scanf("%s", nama);  // baca 1 kata (tanpa spasi)
+
+    printf("Halo, %s! Selamat belajar Git & VSCode!\n", nama);
 
     return 0;
 }
