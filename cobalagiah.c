849274a2592f993f2224a6fdea491f00f977ee1a
ihsansyahni <ihsansyahn@mhs.usk.ac.id>
2025-11-10 11:43:59 +0700

    tes

--- cobalagiah.c
+++ cobalagiah.c
@@ -3,7 +3,7 @@
 int main() {
     char nama[50];
 
-    printf("Masukkan nama kamu: ");
+    printf("Masukkan nama kamu: haloo ;) ");  // kalo ;) berarti berhasil ;) 
     scanf("%s", nama);  // baca 1 kata (tanpa spasi)
 
     printf("Halo, %s! Selamat belajar Git & VSCode!\n", nama);
